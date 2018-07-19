4ece0b615a6a57ff2d92ccb6ef6a548f95019b5e
Rahel Ahmed <RahelA@transportinnovation.co.uk>
2018-07-19 10:53:38 +0100

    adding the files to git

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+/*
+ *	@author: rchow
+ *	Temporary redirect to public folder
+ *	on Production serve dir should be /public
+*/
+
+header('Location: /Artisan/public');
+
---
+++ resources/views/index.blade.php
@@ -0,0 +1,17 @@
+
+
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+<head id="index-fds">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title></title>
+    <!-- hook the Top JS libs -->
+    <!-- hook the CSS SASS/LESS files -->
+    <!-- Use webpack to organise the Assets -->
+    <!-- Ideally also use the ReactJs Library -->
+</head
+<body id="">
+</body>
+</html>
